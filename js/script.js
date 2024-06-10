b46ae83cf09b512f9738f5679e2c4cd97e99924a
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-06-10 18:07:02 +0000

    nsnsg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames al sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames al sharoa
+// Created on: may 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-JS/sw.js", {
+    scope: "/ICS2O-Unit5-04-JS/",
+  })
+}
+
+/**
+ * This function displays checks if user is eligible for the discount.
+ */
+function buttonOnClick() {
+  //input
+  const day = document.getElementById("day").value
+  const age = document.getElementById("age").value
+
+  if (day == "Tuesday" || day == "Thursday" || (age > 12 && age < 25)) {
+    document.getElementById("eligibility").innerHTML =
+      "You are eligible for the discount."
+  } else {
+    document.getElementById("eligibility").innerHTML =
+      "You are NOT eligible for the discount."
+  }
+}
